e85cee4f0748925bc8f20f9f4d6796245c172fa6
Rajat mishra <rajatmishra@agvahealthcare.com>
2022-02-07 10:30:02 +0530

    first commit

---
+++ SuctionPump(pwm)/Ams5812_i2c.h
@@ -0,0 +1,102 @@
+/*
+ * Ams5812_i2c.h
+ *
+ * Created: 24-Dec-21 2:56:45 PM
+ *  Author: AGVA
+ */ 
+
+
+#ifndef AMS5812_I2C_H_
+#define AMS5812_I2C_H_
+//Pmax  and  Pmin 
+// const float AMS5812_0000_D_P_MIN = 0.0f;
+// const float AMS5812_0000_D_P_MAX = 0.075f;
+// const float AMS5812_0001_D_P_MIN = 0.0f;
+// const float AMS5812_0001_D_P_MAX = 0.15f;
+// const float AMS5812_0000_D_B_P_MIN = -0.075f;
+// const float AMS5812_0000_D_B_P_MAX = 0.075f;
+// const float AMS5812_0001_D_B_P_MIN = -0.15f;
+// const float AMS5812_0001_D_B_P_MAX = 0.15f;
+// const float AMS5812_0003_D_P_MIN = 0.0f;
+// const float AMS5812_0003_D_P_MAX = 0.3f;
+// const float AMS5812_0008_D_P_MIN = 0.0f;
+// const float AMS5812_0008_D_P_MAX = 0.8f;
+// const float AMS5812_0015_D_P_MIN = 0.0f;
+// const float AMS5812_0015_D_P_MAX = 1.5f;
+// const float AMS5812_0003_D_B_P_MIN = -0.3f;
+// const float AMS5812_0003_D_B_P_MAX = 0.3f;
+// const float AMS5812_0008_D_B_P_MIN = -0.8f;
+// const float AMS5812_0008_D_B_P_MAX = 0.8f;
+// const float AMS5812_0015_D_B_P_MIN = -1.5f;
+// const float AMS5812_0015_D_B_P_MAX = 1.5f;
+// const float AMS5812_0030_D_P_MIN = 0.0f;
+// const float AMS5812_0030_D_P_MAX = 3.0f;
+// const float AMS5812_0050_D_P_MIN = 0.0f;
+// const float AMS5812_0050_D_P_MAX = 5.0f;
+// const float AMS5812_0150_D_P_MIN = 0.0f;
+// const float AMS5812_0150_D_P_MAX = 15.0f;
+// const float AMS5812_0300_D_P_MIN = 0.0f;
+// const float AMS5812_0300_D_P_MAX = 30.0f;
+// const float AMS5812_0600_D_P_MIN = 0.0f;
+// const float AMS5812_0600_D_P_MAX = 60.0f;
+const float AMS5812_1000_D_P_MIN = 0.0f;
+const float AMS5812_1000_D_P_MAX = 100.0f;
+// const float AMS5812_0030_D_B_P_MIN = -3.0f;
+// const float AMS5812_0030_D_B_P_MAX = 3.0f;
+// const float AMS5812_0050_D_B_P_MIN = -5.0f;
+// const float AMS5812_0050_D_B_P_MAX = 5.0f;
+// const float AMS5812_0150_D_B_P_MIN = -15.0f;
+// const float AMS5812_0150_D_B_P_MAX = 15.0f;
+// const float AMS5812_0150_B_P_MIN = 11.0f;
+// const float AMS5812_0150_B_P_MAX = 17.5f;
+// const float AMS5812_0150_A_P_MIN = 0.0f;
+// const float AMS5812_0150_A_P_MAX = 15.0f;
+// const float AMS5812_0300_A_P_MIN = 0.0f;
+// const float AMS5812_0300_A_P_MAX = 30.0f; // digital output at minimum pressure
+ const int _digOutPmin = 3277;
+ // digital output at maximum pressure
+ const int _digOutPmax = 29491;  //Sensor address   #define Sensor_Address 0x78  #include "I2C_0_AVR128DA64.h"  void Sensor_init(void);  //Initialize pressure Sensor float pressureRead(void); //Read pressure data   void Sensor_init(void) {	 I2C_0_initialization();
+	 I2C_0_master_initialization();
+	 I2C_0_sendAddress(Sensor_Address,1);  //1 for read } float pressureRead(void)
+{
+	uint16_t data = I2C_0_recieveData(); //Reading first byte
+	
+	data = (data)&(0x7F);  //set 8th bit zero
+	
+	data <<= 8;                      //first byte shifted 8 bit
+	
+	data |= I2C_0_recieveData();     //Reading second byte
+	
+	I2C_0_recieveData();   //Reading third byte
+	
+	I2C_0_recieveData();   //Reading forth byte
+	
+	float pressure_count = (float)data;
+	
+	float pressure_in_psi = ((pressure_count-3277)/(26216/AMS5812_1000_D_P_MAX))+AMS5812_1000_D_P_MIN;
+	if(pressure_in_psi > AMS5812_1000_D_P_MAX)
+	{
+		pressure_in_psi = 0.0;
+	}
+	if(pressure_in_psi <= 0)
+	{
+		pressure_in_psi = 0.0;
+	}
+	
+	
+	return pressure_in_psi;
+	
+	_delay_us(10);
+
+	
+}
+
+
+
+
+
+
+
+
+
+#endif /* AMS5812_I2C_H_ */
---
+++ SuctionPump(pwm)/main.c
@@ -0,0 +1,82 @@
+/*
+ * SuctionPump(pwm).c
+ *
+ * Created: 04-Feb-22 10:51:01 AM
+ * Author : AGVA
+ */ 
+
+#define F_CPU							(24000000UL)
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "avr/xmega.h"
+#include "Ams5812_i2c.h"
+#include "UART_1_AVR128DA64 (1).h"
+#include "I2C_0_AVR128DA64.h"
+#include "ADC_AVR128DA64.h"
+
+int  dutyCycle1  = 1500;			// MAX Value	3000
+int  dutyCycle2	 = 1000;			// MAX Value	
+
+void TCA0_PWM_init(void);
+void PWM_Condition1(void);
+void PWM_Condition2(void);
+
+
+int main(void)
+{   _PROTECTED_WRITE (CLKCTRL.OSCHFCTRLA, ((CLKCTRL_FREQSEL_24M_gc)|(CLKCTRL_AUTOTUNE_bm)));			//To increase clock freq
+	TCA0_PWM_init();
+	Sensor_init();
+	USART1_init(9600);
+	ADC0_init();
+	while (1)
+	{   uint8_t i = 200;
+		float pressure = 0.0;
+		
+		while(i)
+		{
+			pressure = pressure + pressureRead();
+			_delay_us(1000);
+			i--;
+		}
+	float	final_pressure = ((pressure/200)*51.7149); //final pressure in mmHg
+	USART1_sendFloat(final_pressure,2);
+	USART1_sendString("voltage");
+	int count = 0;
+	count = ADC0_read(channel_1);
+	USART1_sendInt(count);
+	dutyCycle1 = 0.3663004*count + 1500;
+	TCA0.SINGLE.CMP0 = dutyCycle1;
+		
+	}
+}
+
+void TCA0_PWM_init(void)
+{
+	//sei();
+	/*		FIRST PWM on PA0 PIN		*/
+	// Direction set and set output to high
+	PORTA.DIR |= (1 << 0);
+	PORTA.PIN0CTRL |= (1 << 3);
+	
+	//PORTA.OUTSET |= (1 << 0);
+	TCA0.SINGLE.PER = 3000;     // calculation for 1ms
+	TCA0.SINGLE.CMP0 = dutyCycle1;
+	TCA0.SINGLE.CNT = 0;
+
+	
+	TCA0.SINGLE.CTRLB |= (1 << 4); // compare 0 enable
+	TCA0.SINGLE.CTRLB |= (0x3 << 0); // Single slope PWM
+	TCA0.SINGLE.CTRLA |= (1<<0); //fclk_per 1 and  Enable pwm
+	TCA0.SINGLE.CTRLA |= ((1<<1)|(1<<2)); //fclk_per 8 and  Enable pwm
+	
+	/*		Second PWM on PA1 PIN		*/
+	
+	PORTA.DIR |= (1 << 1);						// Direction set and set output to high
+	PORTA.PIN1CTRL |= (1 << 3);
+	TCA0.SINGLE.CTRLB |= (1 << 5);				// Enable Compare Pa1
+	TCA0.SINGLE.CTRLA |= (1 << 1);				// Enable PWM PA1
+	TCA0.SINGLE.CMP1 = dutyCycle2;					// Duty cycle
+
+}
+
